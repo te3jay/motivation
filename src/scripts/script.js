42960a84c34bad63a47915acc01caa9ea27132fc
Tjeu Roefs <tjeuroefs@hotmail.com>
2019-04-09 14:02:11 +0200

    mobile nav

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,21 +1,16 @@
 console.log('%c Hello from /scripts/script.js ', 'background: lemonchiffon; border: 1px solid #fff');
 console.log('%c Welcome to tris-webpack-boilerplate! ', 'background: lavenderblush; border: 1px solid #fff');
 
-
 // add custom js below
 // external js: masonry.pkgd.js
 $( document ).ready( function() {
 
-$('.grid').masonry({
-    itemSelector: '.grid-item',
-    columnWidth: '.grid-sizer',
-    gutter: 15,
-    percentPosition: true
-});
-
-$(document).ready(function() {
-  $('.image-link').magnificPopup({type:'image'});
-});
+  $('.grid').masonry({
+      itemSelector: '.grid-item',
+      columnWidth: '.grid-sizer',
+      gutter: 15,
+      percentPosition: true
+  });
 
 /*function toggle_visibility(id) {
   var e = document.getElementsByClassName(id);
@@ -25,28 +20,10 @@ $(document).ready(function() {
      e.style.display = 'flex';
 }*/
 
-  $("#mobile-nav").click(function(){
-    $("ul").toggle(500);
-    $("ul").css({
-      'z-index' : '9'
-    });
-    $("li").css({
-      'display' : 'block',
-      'width' : '100%',
-      'text-align' : 'center'
-
-    })
-    $(".navigation").css({
-      'width' : '100%',
-      'justify-content' : 'flex-start',
-      'align-items' : 'flex-start'
-    });
-  });
-
-
 $(".burger").click(function(){
-  $( ".burger" ).toggleClass( "open" )
-})
-
+  $(".burger").toggleClass( "open" )
+});
 
 });
+
+
