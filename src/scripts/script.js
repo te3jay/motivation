1cafd587cd14d7bbbd72ad4450c253c3b77e3ccd
Tjeu Roefs <tjeuroefs@hotmail.com>
2019-04-11 21:55:24 +0200

    cross-browser smooth scroll

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -162,15 +162,14 @@ $( document ).ready( function() {
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
-        scrollTop: $(hash).offset().top
+        scrollTop: $(hash).offset().top - 60
       }, 800, function(){
    
         // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
+       // window.location.hash = hash;
       });
     } // End if
   });
-
   
 });
 
